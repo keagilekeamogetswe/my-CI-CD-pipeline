89e508c4e3dbe79f93d294bd686fc98a6314b4ed
Keamogetswe Keagile <keagile@RDP.swgc.co.za>
2026-05-25 12:31:16 +0200

    Minor: removed the grpcurl line.

--- app/messages/grpc.js
+++ app/messages/grpc.js
@@ -3,7 +3,7 @@ import protoLoader from '@grpc/proto-loader';
 import { Messages } from './service.msg.js';
 
 // Load proto definition
-const packageDef = protoLoader.loadSync('./../proto/messages.proto', {
+const packageDef = protoLoader.loadSync('./../../proto/messages.proto', {
   keepCase: true,
   longs: String,
   enums: String,
