22b253d198e55226def15d4dd19b78a902050334
Keamogetswe Keagile <mrkkeagile@gmail.com>
2026-06-14 20:11:52 +0200

    Chore: cleaning up files

--- aws/script/actions/create_api_for_action.sh
+++
@@ -1,7 +0,0 @@
-aws iam create-open-id-connect-provider --url https://token.actions.githubusercontent.com --client-id-list sts.amazonaws.com   --thumbprint-list 6938fd4d98bab03faadb97b34396831e3780aea1
-
-aws iam create-role --role-name github-actions-eks-deploy   --assume-role-policy-document file://trust-policy.json
-
-aws iam attach-role-policy --role-name github-actions-eks-deploy --policy-arn arn:aws:iam::aws:policy/AmazonEKSClusterPolicy
-
-aws iam attach-role-policy --role-name github-actions-eks-deploy --policy-arn arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser
