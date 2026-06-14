4c81ac2ae83c208197f0c4a5740b0e95f1c30f14
Keamogetswe Keagile <mrkkeagile@gmail.com>
2026-06-14 02:37:02 +0200

    Feature: Session control now deletes a specified session
    This allows users to be logged out and while keeping they might be in possesion of the valid refresh token wherever it is for whenever it shows up.

--- app/user/Repository/Logic/Session.Control.js
+++ app/user/Repository/Logic/Session.Control.js
@@ -38,7 +38,12 @@ export default class SessionControl {
     return insertId!=undefined ? true: false;
   }
   async revokeSession(user_id, jti){
+    const revoke_at = new Date().toISOString().substring(0,19).replace("T"," ")
+    const expires_at = new Date().toISOString().substring(0,19).replace("T"," ")
 
+    const revoke_query = "UPDATE user_session SET revoked_at = ?, expires_at =? WHERE user_id = ? AND  jti = ?"
+    const [result]=await this.mysql_connection.execute(revoke_query, [revoke_at, expires_at, user_id, jti]);
+    return (result.changedRows) ? true: false
   }
   async EndAllSessions(user_id){}
 }
--- tests/user/service/session/repository/session.control.test.js
+++ tests/user/service/session/repository/session.control.test.js
@@ -12,9 +12,12 @@ describe("UserRepository: Testing Session control on DB ", () => {
   let mysql_connection;
   let dial_code_id;
   let user_id;
+  let session_control = new SessionControl(mysql_connection)
+  let mock_decoded_token
   beforeAll(async () => {
     mysql_connection = await Database.getSQLConnection();
     await mysql_connection.beginTransaction();
+    session_control = new SessionControl(mysql_connection)
     const password_hash = await argon2.hash("secret123");
     // Insert a test dial_code so we can reference it
     const [{insertId}] = await mysql_connection.execute(
@@ -22,6 +25,20 @@ describe("UserRepository: Testing Session control on DB ", () => {
       [password_hash]
     );
     user_id = insertId;
+
+    const IPv4 =  "127.0.0.1";
+    const [iat, exp] = [Date.now(),  Date.now() + (1000*60*60*24*20)]
+    const format_date =  Date
+    mock_decoded_token = {
+      jti:"unique_jti",
+      user_id,
+      device_info:"Some device name",
+      IPv4,
+      fp_hash: "fingerprint has",
+      iat,
+      exp,
+      token_hash: "token_hash"
+    }
 
   });
 
@@ -32,41 +49,47 @@ describe("UserRepository: Testing Session control on DB ", () => {
   });
 
   it("should create an a session", async () => {
-    const session_control = new SessionControl(mysql_connection)
-    const IPv4 =  "127.0.0.1";
-    const [iat, exp] = [Date.now(),  Date.now() + (1000*60*60*24*20)]
-    const format_date =  Date
-    const mock_decoded_token = {
-      jti:"unique_jti",
-      user_id,
-      device_info:"Some device name",
-      IPv4,
-      fp_hash: "fingerprint has",
-      iat,
-      exp,
-      token_hash: "token_hash"
-    }
-    const status = await session_control.saveSession(mock_decoded_token);
+    const payload_copy = {...mock_decoded_token}
+    const status = await session_control.saveSession(payload_copy);
     expect(status).toBeTruthy();
     const query_select = "SELECT * FROM user_session WHERE user_id = ?";
     const [[row]] = await mysql_connection.execute(query_select, [user_id]);
     expect(row.id).toBeDefined()
-    expect(mock_decoded_token.iat).toBeUndefined()
-    expect(mock_decoded_token.exp).toBeUndefined()
-    expect(mock_decoded_token.IPv4).toBeUndefined()
-    //The mock_decoded_token gets overwritten when its pass to  session_control.saveSession(mock_decoded_token)
+    expect(payload_copy.iat).toBeUndefined()
+    expect(payload_copy.exp).toBeUndefined()
+    expect(payload_copy.IPv4).toBeUndefined()
+    //The payload_copy gets overwritten when its pass to  session_control.saveSession(payload_copy)
     // Values iat and exp and IPv4 get deleted and replace as follows
-    expect(Date(row.expires_at)).toBe(Date(mock_decoded_token.expires_at))
-    expect(Date(row.created_at)).toBe(Date(mock_decoded_token.created_at))
-    expect(row.ip_address).toBe(IPv4)
+    expect(Date(row.expires_at)).toBe(Date(payload_copy.expires_at))
+    expect(Date(row.created_at)).toBe(Date(payload_copy.created_at))
+    expect(row.ip_address).toBe(payload_copy.ip_address)
     // Testing unchanged key
-    expect(row.user_id).toBe(mock_decoded_token.user_id)
-    expect(row.jti).toBe(mock_decoded_token.jti)
-    expect(row.device_info).toBe(mock_decoded_token.device_info)
-    expect(row.fp_hash).toBe(mock_decoded_token.fp_hash)
+    expect(row.user_id).toBe(payload_copy.user_id)
+    expect(row.jti).toBe(payload_copy.jti)
+    expect(row.device_info).toBe(payload_copy.device_info)
+    expect(row.fp_hash).toBe(payload_copy.fp_hash)
 
-    //Testing the additional values we didnt include in the payload: mock_decoded_token
+    //Testing the additional values we didnt include in the payload: payload_copy
     expect(row.session_type).toBe("primary")
     expect(row.revoked_at).toBeDefined()
+  })
+  it("Should revoke a specific session", async () => {
+    const jti_to_removed = "remove jti";
+    const jti_collection = ["jti_1", jti_to_removed, "Jti_3", "Jti_4"]
+
+    for await (const jit of jti_collection){
+      const payload_copy = {...mock_decoded_token}
+      payload_copy.jti = jit;
+      await session_control.saveSession(payload_copy)
+    }
+    // We're removing jti_to_removed
+    const status = await session_control.revokeSession(user_id, jti_to_removed)
+    //Seletect session of the user_id
+    const select_query = "SELECT * FROM user_session WHERE user_id = ? AND jti= ?"
+    const [[row]] = await mysql_connection.execute(select_query, [user_id, jti_to_removed])
+    expect(status).toBe(true)
+    expect(row.jti).toBeDefined()
+    expect(Date(row.revoked_at)).toStrictEqual(Date(row.expires_at))
+
   })
 });
