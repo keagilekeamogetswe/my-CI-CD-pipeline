bcc8af66f5d7995ed3e91b49671eea186dad4e10
Keamogetswe Keagile <mrkkeagile@gmail.com>
2026-07-01 23:09:12 +0200

    fixup! feat(daemon): initial setup of job executor loop, background worker, and health check test suites

--- tests/deamon/worker/executor.test.js
+++ tests/deamon/worker/executor.test.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-describe("Testing job executer worker", () => {
+describe.sequential("Testing job executer worker", () => {
   let childWorker = null;
 
   afterEach(() => {
