fa2793ec27d3516eef29f037ce534dce0cd8da54
Keamogetswe Keagile <mrkkeagile@gmail.com>
2026-08-20 18:50:11 +0200

    refactor(user-dev) Introducing error message repository. For safe message handling

---
+++ microservices/user/config/login.errors.js
@@ -0,0 +1,5 @@
+export const LoginErrorRepository = {
+  INVALID_CREDENTIALS: "Invalid log in credentials",
+  EMPTY_PASSWORD: "Password cannot be empty",
+  DEVICE_INFOR_STR_ERROR: "Device infor has to be of type string"
+}
