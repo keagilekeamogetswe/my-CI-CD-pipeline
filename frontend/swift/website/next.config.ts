8498b165510d57fab8d17b6c85b3c2f8a5c7f161
Keamogetswe Keagile <mrkkeagile@gmail.com>
2026-09-01 09:29:22 +0200

    feat(frontend) Added localhost:3002/api router rewrites for nextjs.

--- frontend/swift/website/next.config.ts
+++ frontend/swift/website/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: true,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3002/api/:path*", // Proxy to Express backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
