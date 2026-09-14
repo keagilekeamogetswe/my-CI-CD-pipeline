991856aab6d577c1f0f92e838e7f22d363a5582a
Keamogetswe Keagile <87603507+keagilekeamogetswe@users.noreply.github.com>
2026-09-14 19:25:06 +0200

    Add profile picture upload controller and tests (#108)
    * feat(profile-picture) Added a controller responsible for uploading profile pictures
    
    * feat(profile-picture) added profile picture upload controler to the Profile Route.
    
    * test(profile-picture) Added test to test profile picture uploads.

--- microservices/swift-webserver/routes/account/profile.js
+++ microservices/swift-webserver/routes/account/profile.js
@@ -1,6 +1,15 @@
 import express from "express";
 import { ViewProfilePicture } from "./profile/controller/view.profile.pic";
+import {
+  UploadProfilePictureValidator,
+  UploadProfilePicture,
+} from "./profile/controller/post.profile.pic";
 const ProfileRouter = express.Router();
 ProfileRouter.get("/:userId/picture", ViewProfilePicture);
+ProfileRouter.post(
+  "/picture-upload",
+  UploadProfilePictureValidator,
+  UploadProfilePicture,
+);
 
 export default ProfileRouter;
